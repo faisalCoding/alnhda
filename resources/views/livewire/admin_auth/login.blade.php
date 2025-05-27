ad9c29750bc4d7eb71af1013d71dbc177a434726
Faisal <faisalbakhsh07@gmail.com>
2025-05-27 22:36:18 +0300

    test

--- resources/views/livewire/admin_auth/login.blade.php
+++ resources/views/livewire/admin_auth/login.blade.php
@@ -87,7 +87,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
         <!-- Email Address -->
         <flux:input
             wire:model="email "
-            :label="__('your email')"
+            :label="__('your email to')"
             type="email"
             required
             autofocus
