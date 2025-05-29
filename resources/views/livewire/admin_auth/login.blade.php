ce7dbb4de73e511c7fd622474f28590e61b7c530
Faisal <faisalbakhsh07@gmail.com>
2025-05-29 06:30:06 +0300

    test

--- resources/views/livewire/admin_auth/login.blade.php
+++ resources/views/livewire/admin_auth/login.blade.php
@@ -86,7 +86,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
     <form wire:submit="login" class="flex flex-col gap-6">
         <!-- Email Address -->
         <flux:input
-            wire:model="emai l d "
+            wire:model="emai tode "
             :label="__('yourlf')"
             type="email"
             required
