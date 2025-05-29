a8aff503dcdfaf052293cf207f7d1f3aab8b38df
Faisal <faisalbakhsh07@gmail.com>
2025-05-29 09:09:06 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         
         @include('partials.header')
         @livewire('section-project')
-        faisal
+        im faisal
         @include('partials.section_blogs')
         {{-- footer in layouts guest --}}
     </div>
