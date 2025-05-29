60af81e7dfa4447e28ffb47e1eb5e0863c77dfd0
Faisal <faisalbakhsh07@gmail.com>
2025-05-29 08:58:40 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         
         @include('partials.header')
         @livewire('section-project')
-        sopuer ??
+        sopuer so ??
         @include('partials.section_blogs')
         {{-- footer in layouts guest --}}
     </div>
